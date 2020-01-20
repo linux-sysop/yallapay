e4a77fd69156df52f7f785ef5fef6fdd22119e69
mo3az.abdellatif <mo3az.abdellatif@gmail.com>
2020-01-20 12:51:09 +0200

    edits

--- js/main.js
+++ js/main.js
@@ -157,22 +157,24 @@ $(document).ready(function () {
 
     var a = 0;
     $(window).scroll(function () {
-
-        if (a === 0 && $(this).scrollTop() >= ($(".cards-numbers").offset().top) - 700) {
-            $(".loader-span").css("width", "100%");
-            $('.count').each(function () {
-                $(this).prop('Counter', 0).animate({
-                    Counter: $(this).text()
-                }, {
-                    duration: 1000,
-                    easing: 'swing',
-                    step: function (now) {
-                        $(this).text(Math.ceil(now));
-                    }
+        if ($("div").hasClass("cards-numbers")) {
+            if (a === 0 && $(this).scrollTop() >= ($(".cards-numbers").offset().top) - 700) {
+                $(".loader-span").css("width", "100%");
+                $('.count').each(function () {
+                    $(this).prop('Counter', 0).animate({
+                        Counter: $(this).text()
+                    }, {
+                        duration: 1000,
+                        easing: 'swing',
+                        step: function (now) {
+                            $(this).text(Math.ceil(now));
+                        }
+                    });
                 });
-            });
-            a = 1
+                a = 1
+            }
         }
+
 
     });
 
